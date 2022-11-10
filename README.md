3190eb30eebcac28ec3e4ba41e0bfb080bd811e5
Rosabelle Armstead <rosabelle.l.armstead@gmail.com>
2022-11-10 21:39:24 +0000

    Fix funky behaviour

--- README.md
+++
@@ -1,1 +0,0 @@
-# react-rosabelle-art
--- rosabelle-art/src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../App.css';
-
-function Home() {
-	return <div className='page-container'></div>;
-}
-
-export default Home;
