5a597028fe31dfb78d88a2bd0b7f49d7eb1f636d
Rosabelle Armstead <rosabelle.l.armstead@gmail.com>
2022-11-10 01:57:29 +0000

    Nav & Footer
    First-pass nav bar and footer & routes set-up

---
+++ rosabelle-art/src/components/Navbar.js
@@ -0,0 +1,105 @@
+import './Navbar.css';
+
+import Monogram from '../monogram.svg';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+
+function Navbar() {
+	const [click, setClick] = useState(false);
+	//const [click, setButton] = useState(true);
+
+	const handleClick = () => setClick(!click);
+	const closeSidebar = () => setClick(false);
+
+	//window.addEventListener('resize', showButton);
+
+	return (
+		<>
+			<nav className='navbar'>
+				<div className='navbar-container'>
+					{/* Monogram */}
+					<Link
+						to='/'
+						className='navbar-monogram'
+						onClick={closeSidebar}
+					>
+						<img src={Monogram} />
+					</Link>
+
+					{/* Hamburger Menu */}
+					<div
+						className='menu-icon'
+						onClick={handleClick}
+					>
+						<FontAwesomeIcon
+							icon={click ? 'xmark' : 'bars'}
+							fixedWidth
+						/>
+					</div>
+					<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+						<li className='nav-item'>
+							<Link
+								to='/'
+								className='nav-links'
+								onClick={closeSidebar}
+							>
+								home
+								<FontAwesomeIcon
+									className='nav-icon'
+									icon='house-chimney'
+									fixedWidth
+								/>
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link
+								to='/work'
+								className='nav-links'
+								onClick={closeSidebar}
+							>
+								work
+								<FontAwesomeIcon
+									className='nav-icon'
+									icon='briefcase'
+									fixedWidth
+								/>
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link
+								to='/art'
+								className='nav-links'
+								onClick={closeSidebar}
+							>
+								art
+								<FontAwesomeIcon
+									className='nav-icon'
+									icon='palette'
+									fixedWidth
+								/>
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link
+								to='/hobbies'
+								className='nav-links'
+								onClick={closeSidebar}
+							>
+								hobbies
+								<FontAwesomeIcon
+									className='nav-icon'
+									icon='gamepad'
+									fixedWidth
+								/>
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
+}
+
+export default Navbar;
---
+++ rosabelle-art/src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../App.css';
+
+function Home() {
+	return <div className='page-container'></div>;
+}
+
+export default Home;
